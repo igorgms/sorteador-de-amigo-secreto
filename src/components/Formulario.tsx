e5807ff053ac84754d3c9aec99be4a3e64b3f19e
Igor Gomes Firmino <igorgmsf@gmail.com>
2023-04-06 21:56:00 -0300

    aula 1

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,11 @@
+export const Formulario = () => {
+  return (
+    <form action=''>
+      <input
+        type='text'
+        placeholder='Insira os nomes dos participantes'
+      />
+      <button disabled>Adicionar</button>
+    </form>
+  );
+};
